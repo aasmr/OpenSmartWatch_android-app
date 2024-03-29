1673ac37a6173115848389f001219d20e238cfe7
Alexey Smirnov <45847634+aasmr@users.noreply.github.com>
2024-03-29 16:18:14 +0300

    BLE Scan working

--- app/src/main/java/com/aasmr/opensmartwatch/DeviceListActivity.kt
+++ app/src/main/java/com/aasmr/opensmartwatch/DeviceListActivity.kt
@@ -1,34 +1,38 @@
 package com.aasmr.opensmartwatch
 
+
+import android.content.Context
 import android.os.Bundle
-import androidx.activity.ComponentActivity
+import android.content.Intent
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.DropdownMenu
-import androidx.compose.material.DropdownMenuItem
-import androidx.compose.material.ScaffoldState
-import androidx.compose.material.FloatingActionButton
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.TopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.rememberScaffoldState
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+//import androidx.core.content.ContextCompat.startActivity
 import com.aasmr.opensmartwatch.ui.theme.OpenSmartWatchTheme
 
 class DeviceListActivity : AppCompatActivity() {
@@ -39,49 +43,61 @@ class DeviceListActivity : AppCompatActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
                 ) {
-                    DeviceListView()
+                    DeviceListView(context = this@DeviceListActivity)
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DeviceListView(modifier: Modifier = Modifier) {
+fun DeviceListView(modifier: Modifier = Modifier, context: Context? = null) {
     var stateMenu by remember { mutableStateOf(false) }
-    Scaffold(
-        modifier = Modifier.fillMaxSize(),
-        scaffoldState = rememberScaffoldState(),
-        topBar = {
-            TopAppBar(
-                modifier = Modifier,
-                title = {
-                    Text("Список подключённых устройств")
-                },
-                actions = {
-                    IconButton(onClick = { stateMenu = !stateMenu }) {
-                        Icon(
-                            imageVector = Icons.Filled.Menu,
-                            contentDescription = "Открыть меню"
-                        )
-                    }
-                    DropdownMenu(
-                        expanded = stateMenu,
-                        onDismissRequest = { stateMenu = false }
-                    ) {
-                        DropdownMenuItem(onClick = { }) {
-                            Text(text = "Добавить устройство")
+    val snackbarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
+    OpenSmartWatchTheme {
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+            snackbarHost = { SnackbarHost(snackbarHostState) },
+            topBar = {
+                TopAppBar(
+                    modifier = Modifier,
+                    title = {
+                        Text("Подключённые устройства")
+                    },
+                    actions = {
+                        IconButton(onClick = { stateMenu = !stateMenu }) {
+                            Icon(
+                                imageVector = Icons.Filled.Menu,
+                                contentDescription = "Открыть меню"
+                            )
                         }
-                    }
-                },
+                        DropdownMenu(
+                            expanded = stateMenu,
+                            onDismissRequest = { stateMenu = false }
+                        ) {
+                            DropdownMenuItem(
+                                onClick = {
+                                val intent = Intent(context, ScanDeviceActivity::class.java)
+                                context?.startActivity(intent)
+                            },
+                                text = {
+                                    Text("Добавить устройство")
+                                },
+                            )
+                        }
+                    },
+                )
+            }
+        )
+        { innerPadding ->
+            Text(
+                modifier = Modifier
+                    .padding(innerPadding), text = "Нет подключённых устройств"
             )
         }
-    )
-    {
-            innerPadding -> Text(modifier = Modifier
-        .padding(innerPadding), text = "Нет подключённых устройств")
     }
 }
 
--- app/src/main/java/com/aasmr/opensmartwatch/ScanDeviceActivity.kt
+++ app/src/main/java/com/aasmr/opensmartwatch/ScanDeviceActivity.kt
@@ -1,18 +1,64 @@
 package com.aasmr.opensmartwatch
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothManager
+import android.bluetooth.le.BluetoothLeScanner
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanFilter
+import android.bluetooth.le.ScanResult
+import android.bluetooth.le.ScanSettings
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.os.ParcelUuid
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.DropdownMenu
+import androidx.compose.material.DropdownMenuItem
+import androidx.compose.material.ScaffoldState
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.TopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material.Scaffold
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.core.app.ActivityCompat.startActivityForResult
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import com.aasmr.opensmartwatch.ui.theme.OpenSmartWatchTheme
 
-class ScanDeviceActivity : ComponentActivity() {
+class ScanDeviceActivity : AppCompatActivity() {
+
+    private val devices = mutableListOf<BluetoothDevice>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -20,20 +66,128 @@ class ScanDeviceActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+
                 ) {
                     ScanDeviceView()
                 }
             }
         }
+        checkAndRequestPermissions()
+    }
+    // Внутри класса ScanActivity
+
+    // Необходимые разрешения
+    private val requiredPermissions = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        arrayOf(
+            Manifest.permission.BLUETOOTH_SCAN,
+            Manifest.permission.BLUETOOTH_CONNECT,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+    } else {
+        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
+    }
+
+    // Проверка и запрос разрешений
+    private fun checkAndRequestPermissions() {
+        if (requiredPermissions.any { ContextCompat.checkSelfPermission(this, it) != PackageManager.PERMISSION_GRANTED }) {
+            ActivityCompat.requestPermissions(this, requiredPermissions, REQUEST_CODE_PERMISSIONS)
+        } else {
+            // Разрешения уже предоставлены, можно начинать сканирование
+            startBleScan()
+        }
+    }
+
+    // Обработка результата запроса разрешений
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (grantResults.all { it == PackageManager.PERMISSION_GRANTED }) {
+                // Все разрешения предоставлены, можно начинать сканирование
+                startBleScan()
+            } else {
+                // Не все разрешения предоставлены, показать сообщение об ошибке
+                //Toast.makeText(this, "Необходимо предоставить все разрешения", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    // Константа для идентификации запроса разрешений
+    private companion object {
+        const val REQUEST_CODE_PERMISSIONS = 100
+    }
+    @SuppressLint("MissingPermission")
+    private fun startBleScan() {
+        val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
+
+        if (bluetoothAdapter?.isEnabled == true) {
+            val scanner = bluetoothAdapter.bluetoothLeScanner
+
+            // Настройки сканирования
+            val scanSettings = ScanSettings.Builder()
+                .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
+                .build()
+
+            // Фильтр для сканирования (необязательно)
+            val scanFilters: List<ScanFilter>? = null
+
+            // Запуск сканирования
+            scanner.startScan(scanFilters, scanSettings, scanCallback)
+        } else {
+            // Bluetooth выключен, запросить включение
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+        }
+    }
+
+    // Callback для обработки найденных устройств
+    private val scanCallback = object : ScanCallback() {
+        override fun onScanResult(callbackType: Int, result: ScanResult) {
+            val device = result.device
+            if (!devices.contains(device)) {
+                devices.add(device)
+                // Обновить UI (например, вызвать recomposition)
+            }
+        }
+    }
+}
+@Composable
+fun ScanDeviceView(modifier: Modifier = Modifier) {
+    val snackbarHostState = remember { SnackbarHostState() }
+    OpenSmartWatchTheme {
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+            snackbarHost = { SnackbarHost(snackbarHostState) },
+            topBar = {
+                TopAppBar(
+                    modifier = Modifier,
+                    title = {
+                        Text("Добавление нового устройства")
+                    },
+                    navigationIcon = {
+                        IconButton(onClick = {}) {
+                            Icon(
+                                imageVector = Icons.Filled.ArrowBack,
+                                contentDescription = "Назад"
+                            )
+                        }
+                    },
+                )
+            }
+        )
+        { innerPadding ->
+            Text(
+                modifier = Modifier
+                    .padding(innerPadding), text = "Нет подключённых устройств"
+            )
+        }
     }
 }
 
-@Composable
-fun ScanDeviceView(modifier: Modifier = Modifier) {
-}
-
-@Preview(showBackground = true)
+@Preview(showBackground = true,
+    showSystemUi = true)
 @Composable
 fun ScanDeviceViewPreview() {
     OpenSmartWatchTheme {
